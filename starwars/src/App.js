8b1d64acbb449582a96153309ffb69086e1902f0
Milo Rastgoo <hello@informagician.com>
2019-12-13 11:51:18 -0500

    information rendered, moving to style

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,7 +4,7 @@ import Card from './components/card'
 const axios = require('axios');
 
 const App = () => {
-  let [people, setPeople] = useState();
+  let [people, setPeople] = useState([]);
   let [page] = useState(1);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -15,20 +15,22 @@ const App = () => {
   useEffect(() => {
     axios.get(`https://swapi.co/api/people/?page=${page}`)
     .then(response => {
-      console.log(response);
-      people = response.data.results;
-      people.map(item => {
-
-      })
+      //console.log(response);
+      let characters = response.data.results;
+      setPeople(characters);
     })
     .catch(error => {
-      console.log('RoB0t 8rOk3N!');
+      console.log('RoB0t 8rOk3N!', error);
     })
   }, [])
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-    <Card />
+      <div className="container">
+        {people.map(item => {
+          return <Card key={item} name={item.name} height={item.height} />;
+        })}
+      </div>
     </div>
   );
 }
--- starwars/src/components/card.js
+++ starwars/src/components/card.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Card() {
-    return null;
+const Card = props => {
+    
+    return(
+    <div className="card">
+        <h2>Name: {props.name}</h2>
+        <h3>Hight: {props.height}</h3>
+        {/* <h3>Hair Color: {props.hair_color}</h3>
+        <h3>Birth year: {props.birth_year}</h3> */}
+    </div>
+    )
 }
 
 export default Card;
